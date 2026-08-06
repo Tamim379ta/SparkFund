17ad79a0a50e53cc1b3cbe29e529b73917b086f1
Tamim379ta <tamimthasan55@gmail.com>
2026-08-06 16:22:17 +0600

    update auth-client file

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,5 +4,17 @@ export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   fetchOptions: {
     credentials: "include",
+    onRequest: (ctx) => {
+      const token = localStorage.getItem("sparkfund_token");
+      if (token) {
+        ctx.headers.set("Authorization", `Bearer ${token}`);
+      }
+    },
+    onResponse: async (ctx) => {
+      const token = ctx.response.headers.get("set-auth-token");
+      if (token) {
+        localStorage.setItem("sparkfund_token", token);
+      }
+    },
   },
 });
